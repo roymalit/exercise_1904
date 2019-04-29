c2be668bb18d3d5ee0d7a9abf65876a0eafb28ef
Roy M <roymalit@hotmail.com>
2019-04-29 06:51:17 +0100

    'format' method implemented

--- src/dateFormatter.js
+++ src/dateFormatter.js
@@ -1,3 +1,12 @@
 export const format = (dateToFormatTimeMillis, systemDateTimeMillis) => {
-  return '';
+
+	// Create new date objects that don't take time into account
+  	var d1 = new Date(dateToFormatTimeMillis).setHours(0,0,0,0);
+	var d2 = new Date (systemDateTimeMillis).setHours(0,0,0,0);
+
+	if (d1 == d2) {
+		return 'TODAY';
+	} else {
+		return 'NOT TODAY';
+	}
 };
